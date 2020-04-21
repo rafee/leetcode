960613594f6f479a004e5992bb2fac3db94f575e
Mohammad Ishfaque Jahan Rafee <3380056+rafee@users.noreply.github.com>
2020-04-21 12:59:31 -0400

    Problem 278 done

---
+++ 278.first-bad-version.go
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode id=278 lang=golang
+ *
+ * [278] First Bad Version
+ *
+ * https://leetcode.com/problems/first-bad-version/description/
+ *
+ * algorithms
+ * Easy (32.25%)
+ * Likes:    1079
+ * Dislikes: 585
+ * Total Accepted:    318.5K
+ * Total Submissions: 946.2K
+ * Testcase Example:  '5\n4'
+ *
+ * You are a product manager and currently leading a team to develop a new
+ * product. Unfortunately, the latest version of your product fails the quality
+ * check. Since each version is developed based on the previous version, all
+ * the versions after a bad version are also bad.
+ *
+ * Suppose you have n versions [1, 2, ..., n] and you want to find out the
+ * first bad one, which causes all the following ones to be bad.
+ *
+ * You are given an API bool isBadVersion(version) which will return whether
+ * version is bad. Implement a function to find the first bad version. You
+ * should minimize the number of calls to the API.
+ *
+ * Example:
+ *
+ *
+ * Given n = 5, and version = 4 is the first bad version.
+ *
+ * call isBadVersion(3) -> false
+ * call isBadVersion(5) -> true
+ * call isBadVersion(4) -> true
+ *
+ * Then 4 is the first bad version.
+ *
+ *
+ */
+
+package leetcode
+
+// @lc code=start
+/**
+ * Forward declaration of isBadVersion API.
+ * @param   version   your guess about first bad version
+ * @return 	 	      true if current version is bad
+ *			          false if current version is good
+ * func isBadVersion(version int) bool;
+ */
+
+func firstBadVersion(n int) int {
+	good, bad := 1, n
+	for good < bad {
+		mid := (good + bad) / 2
+		if isBadVersion(mid) {
+			bad = mid
+		} else {
+			good = mid + 1
+		}
+	}
+	return good
+}
+
+func isBadVersion(n int) bool {
+	return false
+}
+
+// @lc code=end
