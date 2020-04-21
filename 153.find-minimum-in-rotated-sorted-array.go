5dc6c88465b95ad616af315776cd648a04f8cf3f
Mohammad Ishfaque Jahan Rafee <3380056+rafee@users.noreply.github.com>
2020-04-21 15:16:29 -0400

    Problem 153 & 162 solved

---
+++ 153.find-minimum-in-rotated-sorted-array.go
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode id=153 lang=golang
+ *
+ * [153] Find Minimum in Rotated Sorted Array
+ *
+ * https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
+ *
+ * algorithms
+ * Medium (43.94%)
+ * Likes:    1770
+ * Dislikes: 217
+ * Total Accepted:    399.7K
+ * Total Submissions: 900.4K
+ * Testcase Example:  '[3,4,5,1,2]'
+ *
+ * Suppose an array sorted in ascending order is rotated at some pivot unknown
+ * to you beforehand.
+ *
+ * (i.e.,  [0,1,2,4,5,6,7] might become  [4,5,6,7,0,1,2]).
+ *
+ * Find the minimum element.
+ *
+ * You may assume no duplicate exists in the array.
+ *
+ * Example 1:
+ *
+ *
+ * Input: [3,4,5,1,2]
+ * Output: 1
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: [4,5,6,7,0,1,2]
+ * Output: 0
+ *
+ *
+ */
+
+package leetcode
+
+// @lc code=start
+func findMin(nums []int) int {
+	start, end := 0, len(nums)-1
+	for start < end {
+		mid := (start + end) / 2
+		if nums[mid] < nums[end] {
+			end = mid
+		} else if nums[mid] > nums[end] {
+			start = mid + 1
+		}
+	}
+	return nums[start]
+}
+
+// @lc code=end
---
+++ 278.first-bad-version_test.go
@@ -0,0 +1,84 @@
+/*
+ * @lc app=leetcode id=278 lang=golang
+ *
+ * [278] First Bad Version
+ *
+ * https://leetcode.com/problems/first-bad-version/description/
+ *
+ * algorithms
+ * Easy (32.25%)
+ * Likes:    1079
+ * Dislikes: 585
+ * Total Accepted:    318.5K
+ * Total Submissions: 946.2K
+ * Testcase Example:  '5\n4'
+ *
+ * You are a product manager and currently leading a team to develop a new
+ * product. Unfortunately, the latest version of your product fails the quality
+ * check. Since each version is developed based on the previous version, all
+ * the versions after a bad version are also bad.
+ *
+ * Suppose you have n versions [1, 2, ..., n] and you want to find out the
+ * first bad one, which causes all the following ones to be bad.
+ *
+ * You are given an API bool isBadVersion(version) which will return whether
+ * version is bad. Implement a function to find the first bad version. You
+ * should minimize the number of calls to the API.
+ *
+ * Example:
+ *
+ *
+ * Given n = 5, and version = 4 is the first bad version.
+ *
+ * call isBadVersion(3) -> false
+ * call isBadVersion(5) -> true
+ * call isBadVersion(4) -> true
+ *
+ * Then 4 is the first bad version.
+ *
+ *
+ */
+
+package leetcode
+
+import "testing"
+
+func Test_firstBadVersion(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := firstBadVersion(tt.args.n); got != tt.want {
+				t.Errorf("firstBadVersion() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_isBadVersion(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isBadVersion(tt.args.n); got != tt.want {
+				t.Errorf("isBadVersion() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
