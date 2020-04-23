3647b4f2132bd11a65ba2c5c86ca02f3e9dc4b3c
Mohammad Ishfaque Jahan Rafee <3380056+rafee@users.noreply.github.com>
2020-04-23 09:13:02 -0400

    Problem 201

---
+++ 201.bitwise-and-of-numbers-range_test.go
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode id=201 lang=golang
+ *
+ * [201] Bitwise AND of Numbers Range
+ *
+ * https://leetcode.com/problems/bitwise-and-of-numbers-range/description/
+ *
+ * algorithms
+ * Medium (36.89%)
+ * Likes:    695
+ * Dislikes: 95
+ * Total Accepted:    110.3K
+ * Total Submissions: 293.8K
+ * Testcase Example:  '5\n7'
+ *
+ * Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise AND
+ * of all numbers in this range, inclusive.
+ *
+ * Example 1:
+ *
+ *
+ * Input: [5,7]
+ * Output: 4
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: [0,1]
+ * Output: 0
+ */
+
+package leetcode
+
+import "testing"
+
+func Test_rangeBitwiseAnd(t *testing.T) {
+	type args struct {
+		m int
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "Test 1",
+			args: args{5, 7},
+			want: 4,
+		},
+		{
+			name: "Test 2",
+			args: args{0, 1},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := rangeBitwiseAnd(tt.args.m, tt.args.n); got != tt.want {
+				t.Errorf("rangeBitwiseAnd() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
