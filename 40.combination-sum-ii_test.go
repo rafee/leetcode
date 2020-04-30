36a05a7691e15beb9527ac035daa166a30fa1d87
Mohammad Ishfaque Jahan Rafee <3380056+rafee@users.noreply.github.com>
2020-04-30 02:07:25 -0400

    Problem 40 solved

---
+++ 40.combination-sum-ii_test.go
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode id=40 lang=golang
+ *
+ * [40] Combination Sum II
+ *
+ * https://leetcode.com/problems/combination-sum-ii/description/
+ *
+ * algorithms
+ * Medium (44.64%)
+ * Likes:    1494
+ * Dislikes: 56
+ * Total Accepted:    302.5K
+ * Total Submissions: 651.3K
+ * Testcase Example:  '[10,1,2,7,6,1,5]\n8'
+ *
+ * Given a collection of candidate numbers (candidates) and a target number
+ * (target), find all unique combinations in candidates where the candidate
+ * numbers sums to target.
+ *
+ * Each number in candidates may only be used once in the combination.
+ *
+ * Note:
+ *
+ *
+ * All numbers (including target) will be positive integers.
+ * The solution set must not contain duplicate combinations.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: candidates = [10,1,2,7,6,1,5], target = 8,
+ * A solution set is:
+ * [
+ * ⁠ [1, 7],
+ * ⁠ [1, 2, 5],
+ * ⁠ [2, 6],
+ * ⁠ [1, 1, 6]
+ * ]
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: candidates = [2,5,2,1,2], target = 5,
+ * A solution set is:
+ * [
+ * [1,2,2],
+ * [5]
+ * ]
+ *
+ *
+ */
+
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_combinationSum2(t *testing.T) {
+	type args struct {
+		candidates []int
+		target     int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			name: "Test 1",
+			args: args{[]int{10, 1, 2, 7, 6, 1, 5}, 8},
+			want: [][]int{{1, 1, 6}, {1, 2, 5}, {1, 7}, {2, 6}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := combinationSum2(tt.args.candidates, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("combinationSum2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
