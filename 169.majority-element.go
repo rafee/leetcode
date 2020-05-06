b7d42f694d3b73af492379f7d1c89630d86362f5
Mohammad Ishfaque Jahan Rafee <3380056+rafee@users.noreply.github.com>
2020-05-06 12:33:45 -0400

    169 solved

---
+++ 169.majority-element.go
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode id=169 lang=golang
+ *
+ * [169] Majority Element
+ *
+ * https://leetcode.com/problems/majority-element/description/
+ *
+ * algorithms
+ * Easy (55.12%)
+ * Likes:    2826
+ * Dislikes: 212
+ * Total Accepted:    577.9K
+ * Total Submissions: 1M
+ * Testcase Example:  '[3,2,3]'
+ *
+ * Given an array of size n, find the majority element. The majority element is
+ * the element that appears more than ⌊ n/2 ⌋ times.
+ *
+ * You may assume that the array is non-empty and the majority element always
+ * exist in the array.
+ *
+ * Example 1:
+ *
+ *
+ * Input: [3,2,3]
+ * Output: 3
+ *
+ * Example 2:
+ *
+ *
+ * Input: [2,2,1,1,1,2,2]
+ * Output: 2
+ *
+ *
+ */
+
+package leetcode
+
+// @lc code=start
+func majorityElement(nums []int) int {
+	numMap := make(map[int]int, 0)
+	maxCount := len(nums) / 2
+	for _, num := range nums {
+		numMap[num]++
+		if numMap[num] > maxCount {
+			return num
+		}
+	}
+	return -1
+}
+
+// @lc code=end
