99cb39ffb399cdb6e4ded56496c1ad5d8f9270aa
Mohammad Ishfaque Jahan Rafee <3380056+rafee@users.noreply.github.com>
2020-05-07 12:57:41 -0400

    993 solved

---
+++ 993.cousins-in-binary-tree_test.go
@@ -0,0 +1,98 @@
+/*
+ * @lc app=leetcode id=993 lang=golang
+ *
+ * [993] Cousins in Binary Tree
+ *
+ * https://leetcode.com/problems/cousins-in-binary-tree/description/
+ *
+ * algorithms
+ * Easy (51.97%)
+ * Likes:    599
+ * Dislikes: 33
+ * Total Accepted:    61.7K
+ * Total Submissions: 116.6K
+ * Testcase Example:  '[1,2,3,4]\n4\n3'
+ *
+ * In a binary tree, the root node is at depth 0, and children of each depth k
+ * node are at depth k+1.
+ *
+ * Two nodes of a binary tree are cousins if they have the same depth, but have
+ * different parents.
+ *
+ * We are given the root of a binary tree with unique values, and the values x
+ * and y of two different nodes in the tree.
+ *
+ * Return true if and only if the nodes corresponding to the values x and y are
+ * cousins.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ *
+ * Input: root = [1,2,3,4], x = 4, y = 3
+ * Output: false
+ *
+ *
+ *
+ * Example 2:
+ *
+ *
+ *
+ * Input: root = [1,2,3,null,4,null,5], x = 5, y = 4
+ * Output: true
+ *
+ *
+ *
+ * Example 3:
+ *
+ *
+ *
+ *
+ * Input: root = [1,2,3,null,4], x = 2, y = 3
+ * Output: false
+ *
+ *
+ *
+ *
+ *
+ * Note:
+ *
+ *
+ * The number of nodes in the tree will be between 2 and 100.
+ * Each node has a unique integer value from 1 to 100.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+package leetcode
+
+import "testing"
+
+func Test_isCousins(t *testing.T) {
+	type args struct {
+		root *TreeNode
+		x    int
+		y    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isCousins(tt.args.root, tt.args.x, tt.args.y); got != tt.want {
+				t.Errorf("isCousins() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
