c92dd418cce494b4257328b24a04d7a097e0cf29
Mohammad Ishfaque Jahan Rafee <3380056+rafee@users.noreply.github.com>
2020-05-10 10:39:02 -0400

    Problem 997 Solved

---
+++ 997.find-the-town-judge.go
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=997 lang=golang
+ *
+ * [997] Find the Town Judge
+ */
+
+package leetcode
+
+// @lc code=start
+func findJudge(N int, trust [][]int) int {
+	degree := make([]int, N+1)
+	for _, pair := range trust {
+		degree[pair[0]]--
+		degree[pair[1]]++
+	}
+
+	for i := 1; i <= N; i++ {
+		if degree[i] == (N - 1) {
+			return i
+		}
+	}
+
+	return -1
+}
+
+// @lc code=end
