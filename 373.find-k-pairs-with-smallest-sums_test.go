9a552290fe555eb8d22c3dcbd66f5b9ab17c0d13
Mohammad Ishfaque Jahan Rafee <3380056+rafee@users.noreply.github.com>
2020-05-23 15:35:11 -0400

    Problem 373 Solved

---
+++ 373.find-k-pairs-with-smallest-sums_test.go
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=373 lang=golang
+ *
+ * [373] Find K Pairs with Smallest Sums
+ */
+
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_kSmallestPairs(t *testing.T) {
+	type args struct {
+		nums1 []int
+		nums2 []int
+		k     int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			name: "Test 1",
+			args: args{[]int{1, 7, 11}, []int{2, 4, 6}, 3},
+			want: [][]int{{1, 2}, {1, 4}, {1, 6}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := kSmallestPairs(tt.args.nums1, tt.args.nums2, tt.args.k); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("kSmallestPairs() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
