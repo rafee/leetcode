9dcca0ad495427203987e49d6a7bac677f87b9b0
Mohammad Ishfaque Jahan Rafee <3380056+rafee@users.noreply.github.com>
2020-05-31 23:35:00 -0400

    821 Solved

---
+++ 821.shortest-distance-to-a-character.go
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=821 lang=golang
+ *
+ * [821] Shortest Distance to a Character
+ */
+
+package leetcode
+
+// @lc code=start
+func shortestToChar(S string, C byte) []int {
+	dist := make([]int, len(S))
+	pos := -len(S)
+	for i := 0; i < len(S); i++ {
+		if S[i] == C {
+			pos = i
+		}
+		dist[i] = i - pos
+	}
+
+	pos = 2 * len(S)
+	for i := len(S) - 1; i >= 0; i-- {
+		if S[i] == C {
+			pos = i
+		}
+		dist[i] = min(dist[i], pos-i)
+	}
+
+	return dist
+}
+
+// @lc code=end
