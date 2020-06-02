3c65a9099935ba4742c5462af21d16bd569852f8
Mohammad Ishfaque Jahan Rafee <3380056+rafee@users.noreply.github.com>
2020-06-02 13:30:18 -0400

    Problem 226, 237 Solved

---
+++ 237.delete-node-in-a-linked-list.go
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=237 lang=golang
+ *
+ * [237] Delete Node in a Linked List
+ */
+
+package leetcode
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteNode(node *ListNode) {
+	if node == nil {
+		return
+	}
+	pre := node
+	for node.Next != nil {
+		node.Val = node.Next.Val
+		pre = node
+		node = node.Next
+	}
+	pre.Next = nil
+}
+
+// @lc code=end
