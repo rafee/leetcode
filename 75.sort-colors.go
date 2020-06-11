744053a4a3c5a1c1c3435c27f99cedfe7f40544b
Mohammad Ishfaque Jahan Rafee <3380056+rafee@users.noreply.github.com>
2020-06-11 10:07:38 -0400

    Problem 75 solved

---
+++ 75.sort-colors.go
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=75 lang=golang
+ *
+ * [75] Sort Colors
+ */
+
+package leetcode
+
+// @lc code=start
+func sortColors(nums []int) {
+	red, white, blue := 0, 0, len(nums)-1
+	for white <= blue {
+		if nums[white] == 0 {
+			nums[red], nums[white] = nums[white], nums[red]
+			red++
+			white++
+		} else if nums[white] == 1 {
+			white++
+		} else {
+			nums[blue], nums[white] = nums[white], nums[blue]
+			blue--
+		}
+	}
+}
+
+// @lc code=end
