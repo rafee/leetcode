170688513b319e1e688639ca8ef2cc894a78f719
Mohammad Ishfaque Jahan Rafee <3380056+rafee@users.noreply.github.com>
2020-06-14 15:00:07 -0400

    395 and 787 not solved

---
+++ 787.cheapest-flights-within-k-stops.go
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=787 lang=golang
+ *
+ * [787] Cheapest Flights Within K Stops
+ */
+
+package leetcode
+
+// @lc code=start
+func findCheapestPrice(n int, flights [][]int, src int, dst int, K int) int {
+	cost := 0
+	return cost
+}
+
+// @lc code=end
