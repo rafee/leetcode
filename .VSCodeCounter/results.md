bb084c73e64ee837ba531c258b5dbaa207c98d6e
Mohammad Ishfaque Jahan Rafee <3380056+rafee@users.noreply.github.com>
2020-06-17 10:27:10 -0400

    problem 130, reduce input parameter.Add codemetric

---
+++ .VSCodeCounter/results.md
@@ -0,0 +1,21 @@
+# Summary
+
+Date : 2020-06-16 17:05:28
+
+Directory /home/rafee/go/src/github.com/rafee/leetcode
+
+Total : 394 files,  26446 codes, 0 comments, 2154 blanks, all 28600 lines
+
+[details](details.md)
+
+## Languages
+| language | files | code | comment | blank | total |
+| :--- | ---: | ---: | ---: | ---: | ---: |
+| Go | 394 | 26,446 | 0 | 2,154 | 28,600 |
+
+## Directories
+| path | files | code | comment | blank | total |
+| :--- | ---: | ---: | ---: | ---: | ---: |
+| . | 394 | 26,446 | 0 | 2,154 | 28,600 |
+
+[details](details.md)
--- 130.surrounded-regions.go
+++ 130.surrounded-regions.go
@@ -51,29 +51,29 @@ package leetcode
 
 // @lc code=start
 func solve(board [][]byte) {
-	nX := len(board)
-	if nX == 0 {
+	X := len(board)
+	if X == 0 {
 		return
 	}
-	nY := len(board[0])
-	for i := 0; i < nX; i++ {
+	Y := len(board[0])
+	for i := 0; i < X; i++ {
 		if board[i][0] == 'O' {
-			visit(board, i, 0, nX, nY)
+			visit(board, i, 0)
 		}
-		if board[i][nY-1] == 'O' {
-			visit(board, i, nY-1, nX, nY)
+		if board[i][Y-1] == 'O' {
+			visit(board, i, Y-1)
 		}
 	}
-	for j := 1; j < nY-1; j++ {
+	for j := 1; j < Y-1; j++ {
 		if board[0][j] == 'O' {
-			visit(board, 0, j, nX, nY)
+			visit(board, 0, j)
 		}
-		if board[nX-1][j] == 'O' {
-			visit(board, nX-1, j, nX, nY)
+		if board[X-1][j] == 'O' {
+			visit(board, X-1, j)
 		}
 	}
-	for i := 0; i < nX; i++ {
-		for j := 0; j < nY; j++ {
+	for i := 0; i < X; i++ {
+		for j := 0; j < Y; j++ {
 			if board[i][j] == '*' {
 				board[i][j] = 'O'
 			} else if board[i][j] == 'O' {
@@ -83,16 +83,16 @@ func solve(board [][]byte) {
 	}
 }
 
-func visit(board [][]byte, x, y, X, Y int) {
-	if x < 0 || x > X-1 || y < 0 || y > Y-1 || board[x][y] != 'O' {
+func visit(board [][]byte, x, y int) {
+	if x < 0 || x > len(board)-1 || y < 0 || y > len(board[0])-1 || board[x][y] != 'O' {
 		return
 	}
 
 	board[x][y] = '*'
-	visit(board, x-1, y, X, Y)
-	visit(board, x+1, y, X, Y)
-	visit(board, x, y-1, X, Y)
-	visit(board, x, y+1, X, Y)
+	visit(board, x-1, y)
+	visit(board, x+1, y)
+	visit(board, x, y-1)
+	visit(board, x, y+1)
 }
 
 // @lc code=end
