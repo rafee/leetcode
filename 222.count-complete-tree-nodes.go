e1f74808f91d020e3dcede66170b436effa9b2a4
Mohammad Ishfaque Jahan Rafee <3380056+rafee@users.noreply.github.com>
2020-06-23 13:41:23 -0400

    Problem 222 Simple Solution

---
+++ 222.count-complete-tree-nodes.go
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=222 lang=golang
+ *
+ * [222] Count Complete Tree Nodes
+ */
+
+package leetcode
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func countNodes(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return countNodes(root.Left) + countNodes(root.Right) + 1
+}
+
+// @lc code=end
