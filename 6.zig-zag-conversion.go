3f4d2ec6525bb4b1aa3088ec520400807f3a861a
Mohammad Ishfaque Jahan Rafee <3380056+rafee@users.noreply.github.com>
2020-07-06 13:08:37 -0400

    Problem 6 not done

---
+++ 6.zig-zag-conversion.go
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=6 lang=golang
+ *
+ * [6] ZigZag Conversion
+ */
+
+package leetcode
+
+// @lc code=start
+func convert(s string, numRows int) string {
+	return ""
+}
+
+// @lc code=end
