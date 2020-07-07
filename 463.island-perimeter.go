349db46a422464a33b1104c12ccf036010dfa2e6
Mohammad Ishfaque Jahan Rafee <3380056+rafee@users.noreply.github.com>
2020-07-07 08:36:21 -0400

    Problem 463 resolved

--- 463.island-perimeter.go
+++ 463.island-perimeter.go
@@ -8,7 +8,28 @@ package leetcode
 
 // @lc code=start
 func islandPerimeter(grid [][]int) int {
-	return 0
+	s := 0
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[0]); j++ {
+			if grid[i][j] == 1 {
+				conn := 4
+				if j != 0 && grid[i][j-1] == 1 {
+					conn--
+				}
+				if i != 0 && grid[i-1][j] == 1 {
+					conn--
+				}
+				if j != len(grid[0])-1 && grid[i][j+1] == 1 {
+					conn--
+				}
+				if i != len(grid)-1 && grid[i+1][j] == 1 {
+					conn--
+				}
+				s += conn
+			}
+		}
+	}
+	return s
 }
 
 // @lc code=end
--- 463.island-perimeter_test.go
+++ 463.island-perimeter_test.go
@@ -17,7 +17,14 @@ func Test_islandPerimeter(t *testing.T) {
 		args args
 		want int
 	}{
-		// TODO: Add test cases.
+		{
+			name: "Test 1",
+			args: args{[][]int{{0, 1, 0, 0},
+				{1, 1, 1, 0},
+				{0, 1, 0, 0},
+				{1, 1, 0, 0}}},
+			want: 16,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
