df34779a54c62b51d8942314a5f024857f14fe6c
Mohammad Ishfaque Jahan Rafee <3380056+rafee@users.noreply.github.com>
2020-07-20 15:03:15 -0400

    Problem 167 Solved

---
+++ typescript/src/167.two-sum-ii-input-array-is-sorted.ts
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=167 lang=typescript
+ *
+ * [167] Two Sum II - Input array is sorted
+ */
+
+// @lc code=start
+function twoSumSorted(numbers: number[], target: number): number[] {
+    const numMap: Map<number, number> = new Map()
+    for (let i = 0; i < numbers.length; i++) {
+        if (numMap.has(numbers[i])) {
+            return [numMap.get(numbers[i])!, i + 1]
+            // } else if (numbers[i] > target - numbers[i - 1]) {
+            //     break
+        } else {
+            numMap.set(target - numbers[i], i + 1)
+        }
+    }
+    return []
+};
+// @lc code=end
+
