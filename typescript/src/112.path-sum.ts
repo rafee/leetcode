59ddd2b99be94476d12813ac7816e3c59864bc2e
Mohammad Ishfaque Jahan Rafee <3380056+rafee@users.noreply.github.com>
2020-07-20 12:00:52 -0400

    Problem 112 Solved

---
+++ typescript/src/112.path-sum.ts
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=112 lang=typescript
+ *
+ * [112] Path Sum
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function hasPathSum({ root, sum }: { root: TreeNode | null; sum: number }): boolean {
+    if (root === null) {
+        return false
+    }
+    if (root.left === null && root.right === null) {
+        return root.val === sum
+    }
+    return hasPathSum({ root: root.left, sum: sum - root.val }) || hasPathSum({ root: root.right, sum: sum - root.val })
+};
+// @lc code=end
+
