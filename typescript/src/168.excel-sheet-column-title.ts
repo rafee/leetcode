4759e3ad15ae1841c013b413013c6abd53f42176
Mohammad Ishfaque Jahan Rafee <3380056+rafee@users.noreply.github.com>
2020-07-20 15:26:09 -0400

    168 not solved

---
+++ typescript/src/168.excel-sheet-column-title.ts
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=168 lang=typescript
+ *
+ * [168] Excel Sheet Column Title
+ */
+
+// @lc code=start
+function convertToTitle(n: number): string {
+    let result = ""
+    while (n > 0) {
+        let digit = n % 26
+        n = Math.floor(n / 26)
+        let char = String.fromCharCode(64 + digit)
+        result = char + result
+    }
+    return result
+};
+// @lc code=end
+
