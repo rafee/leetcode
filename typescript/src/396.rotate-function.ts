6d474a78d1be1fe8be3f77beca5d453ee86e8fe9
Mohammad Ishfaque Jahan Rafee <3380056+rafee@users.noreply.github.com>
2020-07-21 05:29:48 -0400

    396 Solved

---
+++ typescript/src/396.rotate-function.ts
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=396 lang=typescript
+ *
+ * [396] Rotate Function
+ */
+
+// @lc code=start
+function maxRotateFunction(A: number[]): number {
+    if (A.length === 0) {
+        return 0
+    }
+    const sum = A.reduce((sum, current) => sum + current, 0)
+    let curSum = 0
+    for (let i = 1; i < A.length; i++) {
+        curSum += (A[i] * i)
+    }
+    let maxSum = Math.max(Number.MIN_SAFE_INTEGER, curSum)
+    for (let i = A.length - 1; i > 0; i--) {
+        curSum += sum
+        curSum -= (A[i] * A.length)
+        maxSum = Math.max(curSum, maxSum)
+    }
+    return maxSum
+};
+// @lc code=end
+
