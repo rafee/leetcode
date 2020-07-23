e5be48eb51bdbd39095ae1ad6f0ce5233ee70147
Mohammad Ishfaque Jahan Rafee <3380056+rafee@users.noreply.github.com>
2020-07-23 00:16:24 -0400

    Testify testcase for Problem 24

--- golang/24.swap-nodes-in-pairs_test.go
+++ golang/24.swap-nodes-in-pairs_test.go
@@ -31,8 +31,9 @@
 package golang
 
 import (
-	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_swapPairs(t *testing.T) {
@@ -44,13 +45,15 @@ func Test_swapPairs(t *testing.T) {
 		args args
 		want *ListNode
 	}{
-		// TODO: Add test cases.
+		{
+			name: "Test 1",
+			args: args{newList([]int{1,2,3,4})},
+			want: newList([]int{2,1,4,3}),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := swapPairs(tt.args.head); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("swapPairs() = %v, want %v", got, tt.want)
-			}
+			assert.Equal(t, tt.want, swapPairs(tt.args.head))
 		})
 	}
 }
