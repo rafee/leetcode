f64fcecb24cadef77878fe21d81515cd1f4bf38c
Mohammad Ishfaque Jahan Rafee <mij.rafee.1344@gmail.com>
2020-10-12 18:53:07 -0600

    Problem 701 iterative and recursive

--- golang/701.insert-into-a-binary-search-tree.go
+++ golang/701.insert-into-a-binary-search-tree.go
@@ -9,12 +9,41 @@ package golang
 // @lc code=start
 
 func insertIntoBST(root *TreeNode, val int) *TreeNode {
+	return insertIntoBSTIterative(root, val)
+}
+
+func insertIntoBSTRecursive(root *TreeNode, val int) *TreeNode {
 	if root == nil {
 		return &TreeNode{Val: val}
 	} else if root.Val > val {
 		root.Left = insertIntoBST(root.Left, val)
 	} else {
 		root.Right = insertIntoBST(root.Right, val)
+	}
+	return root
+}
+
+func insertIntoBSTIterative(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return &TreeNode{Val: val}
+	}
+
+	for iter := root; ; {
+		if iter.Val > val {
+			if iter.Left == nil {
+				iter.Left = &TreeNode{Val: val}
+				break
+			} else {
+				iter = iter.Left
+			}
+		} else {
+			if iter.Right == nil {
+				iter.Right = &TreeNode{Val: val}
+				break
+			} else {
+				iter = iter.Right
+			}
+		}
 	}
 	return root
 }
